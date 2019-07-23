9ede920693888928f897861c141a541f5631c221
Fimics <lpjhblpj@163.com>
2019-07-24 01:08:54 +0800

    c++ const

--- srcplus/base/Basic.cpp
+++ srcplus/base/Basic.cpp
@@ -86,7 +86,7 @@ void print(){
 
 #endif
 
-#if 1
+#if 0
 
 //----------------------using---------------------------
 
@@ -111,12 +111,112 @@ void testUsing(){
 
 #endif
 
+#if 1
+
+//----------------------c++对c语言的增强---------------------------
+
+/**
+ * 1.全局变量的检测增强
+ */
+
+/**
+ * int  a;
+ *int a =10; c可以运行 c++中会报错，重复定义
+ */
+
+/**
+ * 函数检测增强
+ * 1.c++必须有参数类型，和返回值，c语言只是报错
+ * 2.c++调用函数时，参数列表必须匹配
+ *
+ */
+
+int getRect(int w,int h){
+    return  w*h;
+}
+
+/**
+ * 3.类型转换增强
+ */
+void typeCast(){
+    char * p = (char *)malloc(sizeof(char));
+}
+
+/**
+ * 4.c++结构体中可以有函数
+ */
+struct  Person{
+    int age;
+    int addAge();
+};
+
+/**
+ * 5.c++中使用结构体时可以不加struct
+ */
+void testStruct(){
+    struct Person person;//
+    Person person1;
+}
+
+/**
+ *6. bool增强
+ */
+
+void testBool(){
+    bool  falg;
+}
+
+/**
+ * 7.三目运算符的区别？
+ */
+
+
+/**
+ * 8 .const的区别
+ * 1.c语言
+ *   1.1 const 修饰全局变量，是不可以修改的，因为放在常量区，受保护的
+ *   1.2 const 修饰局部变量，是伪常量，可以通过指针修改
+ * 2.c++
+ *  2.1 const 修饰全局变量，是不可以修改的，因为放在常量区，受保护的
+ *  2.2 const 修饰局部变量，是真正的常量，不能修改，为什么不能修改？
+ *      2.2.1 C语言中const修饰的变量是伪常量，编译器是会分配内存的，只要分配内存就可以修改内存中的值
+ *            C++中 const不会分配内存 而是放到符号表中，一个常量符号对应一个值，以后只要找这个符号都是一个值
+ *      2.2.2 const int a = 20; int *p = (int *)&a, *p = 200 为啥不能修改常量值的原理
+ *             1.编译器会临时开辟一块内存空间
+ *             2.int tmp = a//tmp有内存
+ *             3.int *p=(int *)&tmp *p指向的是临时的那块空间，临时空间看不到
+ *
+ * 3.内部链接与外部链接
+ *   3.1 c语言中 const int a 默认是外部链接
+ *   3.2 c++ 中 const int a 默认是内部链接
+ *
+ * 4.const 分配内存情况
+ *   4.1  const int a =10; int *p = (int *)&a 取地址会分配临时内存
+ *   4.2  extern const int a =10 ,加了extern 编译器也分给const 变量分配内存
+ *   4.3  int a =10; const int b = a; 用普通变量初始化const 的变量也会分配内存
+ *   4.4  自定义数据类型 加const也会分配内存
+ *        struct Person{
+ *          int age;
+ *          string name;
+ *        };
+ *
+ *        const Person p1;
+ *
+ * 5. const 与 define区别
+ *   5.1 尽量用const 代替define
+ *   5.2 const 有类型，可进行编译器类型安全检查， #define 无类型，不可进行安全检查
+ *   5.3 const 有作用域，#define 不重视作用域 默认定义处到文件结尾
+ *
+ */
+
+#endif
+
 void basic() {
 #if 0
     print();
     A::a();
     B::a();
+    testUsing();
 #endif
-   testUsing();
 
 }
