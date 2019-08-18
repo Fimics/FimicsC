fd0cb33ef22090c2a553a881dd0be204b189909f
fimics <lpjhblpj@163.com>
2019-08-19 01:26:06 +0800

    能用数组排序

--- srcplus/funtemplate/FunTemplate.cpp
+++ srcplus/funtemplate/FunTemplate.cpp
@@ -33,22 +33,80 @@ void testSwapInt(){
  */
 template <class T>
 
+
 void mySwap(T &a,T &b){
     T tmp = a;
     a = b;
     b=tmp;
 }
 
+template <typename T>
+void mySwap2(){};
 
 void testMySwap(){
     int a = 10;
     int b = 20;
+    char c='c';
     //自动类型推倒
     mySwap(a,b);
+//    mySwap(a,c); 推倒不出类型，所以报错不能运行
+
+    //显式指定类型
+    mySwap<int>(a,b);
+
+    //模板必须指定出T才能使用
+//    mySwap2(); 不能运行
     cout<<"a= "<< a<<" b= "<<b<<endl;
+}
+
+//----------------------------通用数组排序------------------------------------
+
+/**
+ * 从大到小，选择排序
+ */
+
+template <class T>
+void tSwap(T &a,T &b){
+    T tmp =a;
+    a=b;
+    b=tmp;
+}
+
+template <class T>
+void arraySort(T arr[],int len){
+    for (int i = 0; i <len ; i++) {
+        int max =i;
+
+        for (int j = i+1; j <len ; j++) {
+            if(arr[max]<arr[j]){
+                //交换下标
+                max=j;
+            }
+        }
+
+        if(max!=i){
+            //交换数据
+            tSwap(arr[max],arr[i]);
+        }
+    }
+}
+
+template <class T>
+void printArr(T arr[],int len){
+    for (int i = 0; i < len; ++i) {
+        cout<<arr[i]<<endl;
+    }
+}
+
+void testArraySort(){
+   int arr []={4,9,1,3,6,8,0};
+   int len = sizeof(arr)/ sizeof(int);
+   arraySort(arr,len);
+   printArr(arr,len);
 }
 
 void funTemplate(){
 //     testSwapInt();
-testMySwap();
+//testMySwap();
+ testArraySort();
 }
