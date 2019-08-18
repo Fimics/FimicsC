699f57db7c08e560d8af7379789a83a5d9ec9182
fimics <lpjhblpj@163.com>
2019-08-18 11:55:20 +0800

    静态联编与动态联编

--- srcplus/extends/Polymorphism.cpp
+++ srcplus/extends/Polymorphism.cpp
@@ -11,7 +11,13 @@ using namespace std;
 
 class Animal{
 public:
-    void eat(){
+    //静态联编
+//    void eat(){
+//        cout<<"animal eat..."<<endl;
+//    }
+
+   //动态联编
+   virtual void eat(){
         cout<<"animal eat..."<<endl;
     }
 };
@@ -23,11 +29,18 @@ public:
     }
 };
 
+/**
+ * 1.静态联编 outupt ->animal eat...
+ * 调用testEat时，参数的地址已经绑定好了(早绑定，静态联编，编译阶段就确定好了地址)
+ * 2.如果想调用cat的eat,不能提前绑定好函数的地址了，所以需要在运行时候再去确定函数地址(动态联编)
+ *  写法eat()方法改为虚函数，在父类上声名虚函数
+ */
 void testEat(Animal & animal){
     animal.eat();
 }
 
 void polymorphism(){
+    //如果发生了继承关系，编译器允许进行类型转换
     Cat cat ;
     testEat(cat);
 }
