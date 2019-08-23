bb39a26f0259366f53005dcc202818e9747002b9
fimics <lpjhblpj@163.com>
2019-08-24 01:40:01 +0800

    类模板与函数模板

--- srcplus/funtemplate/FunTemplate.cpp
+++ srcplus/funtemplate/FunTemplate.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 /**
@@ -10,19 +12,19 @@ using namespace std;
  */
 
 
-void swapInt(int &a,int &b){
+void swapInt(int &a, int &b) {
     int tmp = a;
-    a=b;
-    b=tmp;
+    a = b;
+    b = tmp;
 }
 
-void swapDouble(double &a, double&b);
+void swapDouble(double &a, double &b);
 
-void testSwapInt(){
-    int a=10;
-    int b =20;
-    swapInt(a,b);
-    cout<<"a= "<<a<<"  b= "<<b<<endl;
+void testSwapInt() {
+    int a = 10;
+    int b = 20;
+    swapInt(a, b);
+    cout << "a= " << a << "  b= " << b << endl;
 }
 
 
@@ -31,32 +33,32 @@ void testSwapInt(){
  * 1.告诉编译器，下面如果出现T不要报错，T是一个能用类型
  * @tparam T
  */
-template <class T>
+template<class T>
 
 
-void mySwap(T &a,T &b){
+void mySwap(T &a, T &b) {
     T tmp = a;
     a = b;
-    b=tmp;
+    b = tmp;
 }
 
-template <typename T>
-void mySwap2(){};
+template<typename T>
+void mySwap2() {};
 
-void testMySwap(){
+void testMySwap() {
     int a = 10;
     int b = 20;
-    char c='c';
+    char c = 'c';
     //自动类型推倒
-    mySwap(a,b);
+    mySwap(a, b);
 //    mySwap(a,c); 推倒不出类型，所以报错不能运行
 
     //显式指定类型
-    mySwap<int>(a,b);
+    mySwap<int>(a, b);
 
     //模板必须指定出T才能使用
 //    mySwap2(); 不能运行
-    cout<<"a= "<< a<<" b= "<<b<<endl;
+    cout << "a= " << a << " b= " << b << endl;
 }
 
 //----------------------------通用数组排序------------------------------------
@@ -65,48 +67,124 @@ void testMySwap(){
  * 从大到小，选择排序
  */
 
-template <class T>
-void tSwap(T &a,T &b){
-    T tmp =a;
-    a=b;
-    b=tmp;
+template<class T>
+void tSwap(T &a, T &b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
 }
 
-template <class T>
-void arraySort(T arr[],int len){
-    for (int i = 0; i <len ; i++) {
-        int max =i;
+template<class T>
+void arraySort(T arr[], int len) {
+    for (int i = 0; i < len; i++) {
+        int max = i;
 
-        for (int j = i+1; j <len ; j++) {
-            if(arr[max]<arr[j]){
+        for (int j = i + 1; j < len; j++) {
+            if (arr[max] < arr[j]) {
                 //交换下标
-                max=j;
+                max = j;
             }
         }
 
-        if(max!=i){
+        if (max != i) {
             //交换数据
-            tSwap(arr[max],arr[i]);
+            tSwap(arr[max], arr[i]);
         }
     }
 }
 
-template <class T>
-void printArr(T arr[],int len){
+template<class T>
+void printArr(T arr[], int len) {
     for (int i = 0; i < len; ++i) {
-        cout<<arr[i]<<endl;
+        cout << arr[i] << endl;
     }
 }
 
-void testArraySort(){
-   int arr []={4,9,1,3,6,8,0};
-   int len = sizeof(arr)/ sizeof(int);
-   arraySort(arr,len);
-   printArr(arr,len);
+void testArraySort() {
+    int arr[] = {4, 9, 1, 3, 6, 8, 0};
+    int len = sizeof(arr) / sizeof(int);
+    arraySort(arr, len);
+    printArr(arr, len);
 }
 
-void funTemplate(){
+//------------------------------模板局限性-------------------------------------
+
+class Person {
+
+
+public:
+    Person(string name, int age) {
+        this->age = age;
+        this->name = name;
+    }
+
+    string name;
+    int age;
+};
+
+template<class T>
+
+bool myCompare(T &a, T &b) {
+//    if (a == b) return true;
+    return false;
+}
+
+////通过具体化自定义类型
+//
+//template <> bool myPersonCompare<Person>(Person &a ,Person &b){
+//    if(a.age==b.age) return true;
+//    return false;
+//}
+
+
+
+void testMyCompare() {
+//    int a=10;
+//    int b =20;
+//    bool  result = myCompare(a,b);
+//    cout<<result<<endl;
+
+    Person p1("tom", 2);
+    Person p2("tom2", 2);
+
+    bool result1 = myCompare(p1, p2);
+}
+
+//------------------------类模板基本使用-------------------------
+
+template<class T, class K=int>
+class Strdent {
+public:
+
+    Strdent(T name, K age) {
+        this->name = name;
+        this->age = age;
+    }
+
+    T name;
+    K age;
+
+    void showInfo() {
+        cout << "name->" << this->name << " age-> " << this->age << endl;
+    }
+};
+
+
+void testClassTemplate() {
+    //类模板不支持自动类型推倒
+//    Strdent p1("zhangsan",100);
+
+
+    Strdent<string,int> s("张三", 100);
+    s.showInfo();
+
+
+}
+
+void funTemplate() {
 //     testSwapInt();
 //testMySwap();
- testArraySort();
+// testArraySort();
+//    testMyCompare();
+    testClassTemplate();
 }
