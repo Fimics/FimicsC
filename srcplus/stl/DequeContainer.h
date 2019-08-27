1506050709d17ea3f16a57a3d0cd6ebebc72636d
fimics <lpjhblpj@163.com>
2019-08-28 01:05:40 +0800

    deque

---
+++ srcplus/stl/DequeContainer.h
@@ -0,0 +1,8 @@
+//
+// Created by lipengju on 2019-08-28.
+//
+
+#ifndef FIMICSCPP_DEQUECONTAINER_H
+#define FIMICSCPP_DEQUECONTAINER_H
+void dequeContainer();
+#endif //FIMICSCPP_DEQUECONTAINER_H
--- srcplus/stl/YStl.cpp
+++ srcplus/stl/YStl.cpp
@@ -6,11 +6,13 @@
 #include "VectorContainer.h"
 #include "VectorContainer1.h"
 #include "StringContainer.h"
+#include "DequeContainer.h"
 using  namespace std;
 
 
 void fstl(){
 //    vectorContainer();
-      vectorContainer1();
+//      vectorContainer1();
 //      stringContainer();
+        dequeContainer();
 }
