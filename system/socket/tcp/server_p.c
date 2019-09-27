8f4ad98e98a66abf8d077a9d37e11aaabc1573fb
fimics <lpjhblpj@163.com>
2019-09-28 03:25:32 +0800

    server_process

---
+++ system/socket/tcp/server_p.c
@@ -0,0 +1,105 @@
+//
+// Created by lipengju on 2019-09-28.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+//进程回收
+void resycle(){
+    pid_t  pid;
+    while ((pid=waitpid(-1,NULL,WNOHANG))>0){
+        printf("child died,pid=%d\n",pid);
+    }
+}
+
+int main(int argc, const char *argv[]) {
+
+    if (argc < 2) {
+        printf("eg: ./a.out port\n");
+        exit(1);
+    }
+
+    struct sockaddr_in serv_addr;
+    socklen_t serv_len = sizeof(serv_addr);
+    int port = atoi(argv[1]);
+
+    //创建套接字
+    int lfd = socket(AF_INET, SOCK_STREAM, 0);
+    //初始化服务器sockaddr_in
+    memset(&serv_addr, 0, serv_len);
+    serv_addr.sin_family = AF_INET; //地址族
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);//监听本机所有IP
+    serv_addr.sin_port = htonl(port);//设置端口
+    //绑定IP和端口
+    bind(lfd, (struct sockaddr *) &serv_addr, serv_len);
+
+    //设置监听的最大个数
+    listen(lfd, 36);
+    printf("start accept...\n");
+
+    //使用信号回收子进程pcb
+    struct sigaction act;
+    act.sa_handler = resycle;
+    act.sa_flags=0;
+    sigemptyset(&act.sa_mask);
+    sigaction(SIGCHLD,&act,NULL);
+
+
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    while (1) {
+        printf("父进程接收连接请求");
+        int cfd = accept(lfd, (struct sockaddr *) &client_addr, &client_len);
+        if (cfd == -1) {
+            perror("accept error\n");
+            exit(1);
+        }
+
+        printf("connect successfully...\n");
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            //child
+//            close(lfd);
+            char ip[64];
+            printf("client-ip :%s,port:%d",
+                    inet_ntop(AF_INET,&client_addr.sin_addr.s_addr,ip, sizeof(ip)),
+                    ntohs(client_addr.sin_port));
+            while (1){
+                char buf [1024];
+                int len = read(cfd,buf, sizeof(buf));
+                if(len==-1){
+                    perror("read error\n");
+                    exit(1);
+                } else if(len==0){
+                    printf("client closed\n");
+                    close(cfd);
+                    break;
+                } else{
+                    printf("recv buf:%s\n",buf);
+                    write(cfd,buf,len);
+                }
+            }
+
+            //子进程退出
+            return 0;
+        } else if (pid > 0) {
+            //parent
+            close(cfd);
+        }
+    }
+
+    return 0;
+}
+
+
